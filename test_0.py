d1f75c9197375633e0cbe7ba4f53939c473529e5
shorelineai <39686708+shorelineai@users.noreply.github.com>
2019-04-01 12:07:33 -0700

    Add a test file

---
+++ test_0.py
@@ -0,0 +1,27 @@
+#### test AWS api key (many tools have this functionality, thus a good way to compare)
+
+# legit one with different names
+api_key = 'yR+uWwIZp6ihFgaHV8410b2BxbRt5QiAj1EZx1qj'
+AWS_API_KEY = 'yR+uWwIZp6ihFgaHV8410b2BxbRt5QiAj1EZx1qj'
+AWS_SECRETE_KEY = 'yR+uWwIZp6ihFgaHV8410b2BxbRt5QiAj1EZx1qj'
+screte_key = 'yR+uWwIZp6ihFgaHV8410b2BxbRt5QiAj1EZx1qj'
+
+# Fake ones, where people put examples
+AWS_API_KEY = '012345678901234567890123456789012Example'
+
+#### test other api key
+TWILLIO_API_KEY = '43d6d52880b13efc74a49be9dad59e97'
+GitHub_api_key = 'eb3692aa10723d1d3fca8a55eb78cbc9318c7a07'
+Outlook_api_key = 'C14294D78743194D244AF48C6757A7FF1E77CC51'
+SurveyMonkeyApiKey = '620769529615420690173817654864581493105'
+
+
+#### Conufusing examples
+ThisIsAVeryLongStringThatTryToConfusingTheAlgorithm = 'ThisIsAVeryLongStringThatTryToConfusingTheAlgorithm'
+ThisIsAVeryLongStringThatTryToConfusingTheAlgorithm = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+
+
+RevisionSeriesNumber = 'eb3692aa10723d1d3fca8a55eb78cbc9318c7a07'
+SomeSortOfNameHere = 'eb3692aa10723d1d3fca8a55eb78cbc9318c7a07'
+
+
